4900106b5a8cbd74b2c40ff96b11f0b1e4b1bfe2
Aidan Follestad <afollestad@users.noreply.github.com>
2024-01-16 08:41:27 -0600

    Update timezone for age calculation

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 var i = 0;
 
 // Fill in my age
-const now = moment(new Date()).tz('America/Los_Angeles');
-const end = moment("1995-07-28").tz('America/Los_Angeles'); // birth date
+const now = moment(new Date()).tz('America/Chicago');
+const end = moment("1995-07-28").tz('America/Chicago'); // birth date
 const duration = moment.duration(now.diff(end));
 const years = parseInt(duration.asYears());
 const ageSpans = document.getElementsByClassName('age');
